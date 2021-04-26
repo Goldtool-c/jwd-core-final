8afe9a23cf492c3f5aa813ec0ef112a5126946a0
goldtool-c <gladyshev.gladyshev@gmail.com>
2021-04-26 21:24:15 +0300

    Show planets

--- src/main/java/com/epam/jwd/core_final/Controller/ShowMapController.java
+++ src/main/java/com/epam/jwd/core_final/Controller/ShowMapController.java
@@ -1,16 +1,42 @@
 package com.epam.jwd.core_final.Controller;
+import com.epam.jwd.core_final.Repository.BaseEntityStorage;
+import com.epam.jwd.core_final.domain.AbstractBaseEntity;
+import com.epam.jwd.core_final.domain.Planet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ShowMapController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowMapController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> nameColumn;
+    private TableColumn<Planet, String> nameColumn;
 
     @FXML
-    private TableColumn<?, ?> xColumn;
+    private TableColumn<Planet, String> xColumn;
 
     @FXML
-    private TableColumn<?, ?> yColumn;
+    private TableColumn<Planet, String> yColumn;
 
+    @FXML
+    private TableView<Planet> planetTable;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Planet> planets = FXCollections.observableArrayList();
+        planets.addAll(BaseEntityStorage.GENERAL.getPlanetStorage());
+        this.xColumn.setCellValueFactory(new PropertyValueFactory<>("x"));
+        this.yColumn.setCellValueFactory(new PropertyValueFactory<>("y"));
+        this.nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        planetTable.getColumns().add(xColumn);
+        planetTable.getColumns().add(yColumn);
+        planetTable.getColumns().add(nameColumn);
+        planetTable.setItems(planets);
+    }
 }
