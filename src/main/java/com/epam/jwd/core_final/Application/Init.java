f389de57c1d7b69b094490ec51e3e6aeff5410df
goldtool-c <gladyshev.gladyshev@gmail.com>
2021-04-28 18:38:10 +0300

    Application init

---
+++ src/main/java/com/epam/jwd/core_final/Application/Init.java
@@ -0,0 +1,57 @@
+package com.epam.jwd.core_final.Application;
+
+import com.epam.jwd.core_final.Main;
+import com.epam.jwd.core_final.Repository.BaseEntityStorage;
+import com.epam.jwd.core_final.converter.ConverterStringToCrewMember;
+import com.epam.jwd.core_final.converter.ConverterStringToMission;
+import com.epam.jwd.core_final.converter.ConverterStringToSpaceMap;
+import com.epam.jwd.core_final.converter.ConverterStringToSpaceship;
+import com.epam.jwd.core_final.domain.ApplicationProperties;
+import com.epam.jwd.core_final.exception.InvalidStateException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public enum Init {
+    GENERAL;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Init.class);
+    public void init(Stage primaryStage)
+    {
+        Parent root = null;
+        try {
+            File sample = new File(".\\src\\main\\java\\com\\epam\\jwd\\core_final\\mainSample.fxml");
+            URL url = new URL("file:/"+sample.getAbsolutePath());
+            root = FXMLLoader.load(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("Main stage initialized");
+        LOGGER.info("Application properties loaded");
+        File file = new File(".\\src\\main\\resources\\"+ ApplicationProperties.GENERAL.input()+"\\"+ ApplicationProperties.GENERAL.crewFileName());
+        try {
+            BaseEntityStorage.GENERAL.setCrewStorage(ConverterStringToCrewMember.GENERAL.convert(file));
+        } catch (InvalidStateException e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("Crewmate storage initialized");
+        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.spaceshipFileName());
+        BaseEntityStorage.GENERAL.setShipStorage(ConverterStringToSpaceship.GENERAL.convert(file));
+        LOGGER.info("Spaceship storage initialized");
+        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.planetMapFilename());
+        BaseEntityStorage.GENERAL.setPlanetStorage(ConverterStringToSpaceMap.GENERAL.convert(file));
+        LOGGER.info("Planet storage initialized");
+        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.missionsFileName());
+        BaseEntityStorage.GENERAL.setFlightMission(ConverterStringToMission.GENERAL.convert(file));
+        LOGGER.info("Mission storage initialized");
+        primaryStage.setTitle("Nasa");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+}
--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -1,5 +1,6 @@
 package com.epam.jwd.core_final;
 
+import com.epam.jwd.core_final.Application.Init;
 import com.epam.jwd.core_final.Repository.BaseEntityStorage;
 import com.epam.jwd.core_final.converter.*;
 import com.epam.jwd.core_final.domain.ApplicationProperties;
@@ -18,38 +19,9 @@ import java.net.URL;
 import java.util.Properties;
 
 public class Main extends javafx.application.Application {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     @Override
     public void start(Stage primaryStage) {
-        Parent root = null;
-        try {
-            File sample = new File(".\\src\\main\\java\\com\\epam\\jwd\\core_final\\mainSample.fxml");
-            URL url = new URL("file:/"+sample.getAbsolutePath());
-            root = FXMLLoader.load(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        LOGGER.info("Main stage initialized");
-        LOGGER.info("Application properties loaded");
-        File file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ ApplicationProperties.GENERAL.crewFileName());
-        try {
-            BaseEntityStorage.GENERAL.setCrewStorage(ConverterStringToCrewMember.GENERAL.convert(file));
-        } catch (InvalidStateException e) {
-            e.printStackTrace();
-        }
-        LOGGER.info("Crewmate storage initialized");
-        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.spaceshipFileName());
-        BaseEntityStorage.GENERAL.setShipStorage(ConverterStringToSpaceship.GENERAL.convert(file));
-        LOGGER.info("Spaceship storage initialized");
-        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.planetMapFilename());
-        BaseEntityStorage.GENERAL.setPlanetStorage(ConverterStringToSpaceMap.GENERAL.convert(file));
-        LOGGER.info("Planet storage initialized");
-        file = new File(".\\src\\main\\resources\\"+ApplicationProperties.GENERAL.input()+"\\"+ApplicationProperties.GENERAL.missionsFileName());
-        BaseEntityStorage.GENERAL.setFlightMission(ConverterStringToMission.GENERAL.convert(file));
-        LOGGER.info("Mission storage initialized");
-        primaryStage.setTitle("Nasa");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+        Init.GENERAL.init(primaryStage);
     }
     public static void main(String[] args) {
         javafx.application.Application.launch(args);
